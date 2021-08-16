9ef537506925c7f0d91332cb9b06a89d2d0bcd56
frothywater <kokoroic@outlook.com>
2021-08-16 18:40:01 +0800

    refactor: Move `command_dict` to `myshell.commands`

--- myshell/commands/__init__.py
+++ myshell/commands/__init__.py
@@ -1,1 +1,23 @@
-__all__ = ["time"]
+from myshell.commands.cd import ChangeDirectoryCommand
+from myshell.commands.clr import ClearCommand
+from myshell.commands.dir import DirectoryInfoCommand
+from myshell.commands.echo import EchoCommand
+from myshell.commands.pwd import PrintWorkingDirectoryCommand
+from myshell.commands.set import SetEnvironCommand
+from myshell.commands.test import TestCommand
+from myshell.commands.time import TimeCommand
+from myshell.commands.umask import UmaskCommand
+from myshell.commands.unset import UnsetEnvironCommand
+
+command_dict = {
+    "time": TimeCommand,
+    "clr": ClearCommand,
+    "pwd": PrintWorkingDirectoryCommand,
+    "echo": EchoCommand,
+    "cd": ChangeDirectoryCommand,
+    "set": SetEnvironCommand,
+    "unset": UnsetEnvironCommand,
+    "umask": UmaskCommand,
+    "dir": DirectoryInfoCommand,
+    "test": TestCommand,
+}
